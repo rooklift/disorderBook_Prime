1f7848cc506ed75508978d7eb8020c26a05c5d65
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-01-27 23:54:07 +0000

    simple WS test program

---
+++ ws_test.py
@@ -0,0 +1,14 @@
+from websocket import create_connection
+
+url = "ws://127.0.0.1:8001/ob/api/ws/HOGEDOGE/venues/TESTEX/tickertape/stocks/FOOBAR"
+ws = create_connection(url)
+
+while 1:
+
+    try:
+        raw_food = ws.recv()
+    except:
+        ws = create_connection(url)
+        continue
+
+    print(raw_food)
